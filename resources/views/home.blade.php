d77d7771214930525bfca52a6697859dde33d33d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-04-06 09:58:46 +0800

    Added Counting at dashboard.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,32 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">Total Articles</div>
+                                <div class="panel-body text-center">
+                                    <h3>{{ $article_count }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">Total Dictionaries</div>
+                                <div class="panel-body text-center">
+                                    <h3>{{ $dictionary_count }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">Total Words</div>
+                                <div class="panel-body text-center">
+                                    <h3>{{ $word_count }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
