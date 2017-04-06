61cb6ff6f69551626690fb623db42bda093abf7f
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-04-06 08:39:55 +0800

    Attach default role for default user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,11 +12,14 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        User::truncate();
         User::create([
             'name'     => 'Amanz',
             'email'    => 'glossari@amanz.my',
             'password' => bcrypt('password'),
-        ]);
+        ])->attachRole(1);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->command->info('Default User Created.');
         $this->command->info('E-mail: glossari@amanz.my');
         $this->command->info('Password: password');
