779f8bc31404789f66d8d94829c593ec6b42b618
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-04-08 12:23:58 +0800

    added date article created in fillable

--- app/Article.php
+++ app/Article.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = [
-        'user_id', 'title', 'article', 'url',
+        'user_id', 'title', 'article', 'url', 'date',
+    ];
+
+    protected $dates = [
+        'date',
     ];
 
     public function paragraphs()
