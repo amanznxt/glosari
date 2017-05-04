c4e9adb52437aa04f782431c6597c81403e8257d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-05-04 09:21:31 +0800

    change btn size

--- resources/views/components/link-modal.blade.php
+++ resources/views/components/link-modal.blade.php
@@ -1,12 +1,14 @@
+<center>
 <div class="btn-group">
-	<a class="btn btn-xs btn-primary"
+	<a class="btn btn-primary"
 		data-toggle="modal"
 		data-target="#{{ $toggle }}"
 		data-dictionary="{{ $dictionary }}"
 		data-name="{{ $name }}">
 		<i class="glyphicon glyphicon-search"></i>
 	</a>
-	<div class="btn btn-xs btn-danger" onclick="deleteDictionary({{ $dictionary }})">
+	<div class="btn btn-danger" onclick="deleteDictionary({{ $dictionary }})">
 		<i class="glyphicon glyphicon-trash"></i>
 	</div>
 </div>
+</center>
