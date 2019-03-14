c671d956fe392d7e89d6d1db5ae2d91bf91cb044
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-03-14 20:51:21 +0800

    Update app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
      */
 
-    'name' => 'Amanz Glossari',
+    'name' => env('APP_NAME', 'Glosari'),
 
     'themes' => [
         'default' => [
